4ad559e3d6611c17c477081dc32d8c8199e6cb36
mnbi <mnbi@users.noreply.github.com>
2020-10-26 17:47:00 +0900

    feat: add Repository#exist?(timestamp)
    - define a new interface in Repository class
    - implement the exist? method in FileSystemRepository class
    - write tests to test FileSystemRepository#exist?
    - bump version 0.4.2 -> 0.4.3

--- lib/textrepo/file_system_repository.rb
+++ lib/textrepo/file_system_repository.rb
@@ -1,27 +1,34 @@
 require 'fileutils'
 
 module Textrepo
+
   ##
   # A concrete class which implements Repository interfaces.  This
   # repository uses the default file system of the operating system as
   # a text storage.
+
   class FileSystemRepository < Repository
+
     ##
     # Repository root.
+
     attr_reader :path
 
     ##
     # Extension of notes sotres in the repository.
+
     attr_reader :extname
 
     ##
     # Default name for the repository which uses when no name is
     # specified in the configuration settings.
+
     FAVORITE_REPOSITORY_NAME = 'notes'
 
     ##
     # Default extension of notes which uses when no extname is
     # specified in the configuration settings.
+
     FAVORITE_EXTNAME = 'md'
 
     ##
@@ -40,7 +47,7 @@ module Textrepo
     # 
     # Default values are set when `repository_name` and `default_extname`
     # were not defined in `conf`.
-    #
+
     def initialize(conf)
       super
       base = conf[:repository_base]
@@ -53,10 +60,10 @@ module Textrepo
     ##
     # Creates a file into the repository, which contains the specified
     # text and is associated to the timestamp.
-    #
+
     # :call-seq:
     #     create(Timestamp, Array) => Timestamp
-    #
+
     def create(timestamp, text)
       abs = abspath(timestamp)
       raise DuplicateTimestampError, timestamp if FileTest.exist?(abs)
@@ -69,10 +76,10 @@ module Textrepo
     ##
     # Reads the file content in the repository.  Then, returns its
     # content.
-    #
+
     # :call-seq:
     #     read(Timestamp) => Array
-    #
+
     def read(timestamp)
       abs = abspath(timestamp)
       raise MissingTimestampError, timestamp unless FileTest.exist?(abs)
@@ -86,10 +93,10 @@ module Textrepo
     ##
     # Updates the file content in the repository.  A new timestamp
     # will be attached to the text.
-    #
+
     # :call-seq:
     #     update(Timestamp, Array) => Timestamp
-    #
+
     def update(timestamp, text)
       raise EmptyTextError if text.empty?
       org_abs = abspath(timestamp)
@@ -108,10 +115,10 @@ module Textrepo
 
     ##
     # Deletes the file in the repository.
-    #
+
     # :call-seq:
     #     delete(Timestamp) => Array
-    #
+
     def delete(timestamp)
       abs = abspath(timestamp)
       raise MissingTimestampError, timestamp unless FileTest.exist?(abs)
@@ -124,10 +131,10 @@ module Textrepo
 
     ##
     # Finds entries of text those timestamp matches the specified pattern.
-    #
+
     # :call-seq:
     #     entries(String = nil) => Array
-    #
+
     def entries(stamp_pattern = nil)
       results = []
 
@@ -158,19 +165,28 @@ module Textrepo
       results
     end
 
+    ##
+    # Check the existence of text which is associated with the given
+    # timestamp.
+
+    # :call-seq:
+    #     exist?(Timestamp) -> true or false
+
+    def exist?(timestamp)
+      FileTest.exist?(abspath(timestamp))
+    end
+
     # :stopdoc:
+
     private
     def abspath(timestamp)
       filename = timestamp_to_pathname(timestamp) + ".#{@extname}"
       File.expand_path(filename, @path)
     end
 
-    ##
-    # ```
     #  %Y   %m %d %H %M %S  suffix        %Y/%m/  %Y%m%d%H%M%S %L
     # "2020-12-30 12:34:56  (0 | nil)" => "2020/12/20201230123456"
     # "2020-12-30 12:34:56  (7)"       => "2020/12/20201230123456_007"
-    # ```
     def timestamp_to_pathname(timestamp)
       yyyy, mo = Timestamp.split_stamp(timestamp.to_s)[0..1]
       File.join(yyyy, mo, timestamp.to_s)
@@ -185,10 +201,6 @@ module Textrepo
 
     def timestamp_str(text_path)
       File.basename(text_path).delete_suffix(".#{@extname}")
-    end
-
-    def exist?(timestamp)
-      FileTest.exist?(abspath(timestamp))
     end
 
     def find_entries(stamp_pattern)
--- lib/textrepo/repository.rb
+++ lib/textrepo/repository.rb
@@ -1,31 +1,72 @@
 module Textrepo
   class Repository
-    attr_reader :type, :name
+
+    ##
+    # Repository type.  It specifies which concrete repository class
+    # will instantiated.  For example, the type `:file_system` specifies
+    # `FileSystemRepository`.
+
+    attr_reader :type
+
+    ##
+    # Repository name.  The usage of the value of `name` depends on a
+    # concrete repository class.  For example, `FileSystemRepository`
+    # uses it as a part of the repository path.
+
+    attr_reader :name
+
+    ##
+    # Create a new repository.  The argument must be an object which
+    # can be accessed like a Hash object.
 
     def initialize(conf)
       @type = conf[:repository_type]
       @name = conf[:repository_name]
     end
 
+    ##
     # Stores text data into the repository with the specified timestamp.
     # Returns the timestamp.
+
+    # :call-seq:
+    #     create(Timestamp, Array) -> Timestamp
+
     def create(timestamp, text); timestamp; end
 
+    ##
     # Reads text data from the repository, which is associated to the
     # timestamp.  Returns an array which contains the text.
+
+    # :call-seq:
+    #     read(Timestamp) -> Array
+
     def read(timestamp); []; end
 
+    ##
     # Updates the content with text in the repository, which is
     # associated to the timestamp.  Returns the timestamp.
+
+    # :call-seq:
+    #     update(Timestamp, Array) -> Timestamp
+
     def update(timestamp, text); timestamp; end
 
+    ##
     # Deletes the content in the repository, which is associated to
     # the timestamp.  Returns an array which contains the deleted text.
+
+    # :call-seq:
+    #     delete(Timestamp) -> Array
+
     def delete(timestamp); []; end
 
+    ##
     # Finds all entries of text those have timestamps which mathes the
     # specified pattern of timestamp.  Returns an array which contains
-    # timestamps.  A pattern must be one of the following:
+    # timestamps.  If none of text was found, an empty array would be
+    # returned.
+    #
+    #  A pattern must be one of the following:
     #
     #     - yyyymoddhhmiss_lll : whole stamp
     #     - yyyymoddhhmiss     : omit millisecond part
@@ -37,17 +78,31 @@ module Textrepo
     # If `stamp_pattern` is omitted, the recent entries will be listed.
     # Then, how many entries are listed depends on the implementaiton
     # of the concrete repository class.
+
+    # :call-seq:
+    #     entries(String) -> Array
+
     def entries(stamp_pattern = nil); []; end
 
+    ##
+    # Check the existence of text which is associated with the given
+    # timestamp.
+    #
+    # :call-seq:
+    #     exist?(Timestamp) -> true or false
+
+    def exist?(timestamp); false; end
   end
 
   require_relative 'file_system_repository'
 
+  ##
   # Returns an instance which derived from Textrepo::Repository class.
-  # `conf` must be a Hash object which has a value of
-  # `:repository_type` and `:repository_name` at least.  Some concrete
-  # class derived from Textrepo::Repository may require more key-value
-  # pairs in `conf`.
+  # `conf` must be an object which can be accessed like a Hash object.
+  # And it must also has a value of `:repository_type` and
+  # `:repository_name` at least.  Some concrete class derived from
+  # Textrepo::Repository may require more key-value pairs in `conf`.
+
   def init(conf)
     type = conf[:repository_type]
     klass_name = type.to_s.split(/_/).map(&:capitalize).join + "Repository"
