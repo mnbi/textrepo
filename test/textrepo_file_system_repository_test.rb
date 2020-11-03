1dc39b286d2f29c96216fe1f81393c66d9adc612
mnbi <mnbi@users.noreply.github.com>
2020-11-03 12:16:05 +0900

    fix: correct typo in code of FileSystemRepository
    - wrong: o.empty -> correct: o.empty?
    - bump version: 0.5.2 -> 0.5.3

--- test/textrepo_file_system_repository_test.rb
+++ test/textrepo_file_system_repository_test.rb
@@ -358,12 +358,26 @@ class TextrepoFileSystemRepositoryTest < Minitest::Test
     refute repo.exist?(stamp)
   end
 
+  # for `search(pattern, timestamp_pattern)`
   def test_it_can_search_word
     assert_search("apple", 1)
   end
 
   def test_it_can_search_regex_pattern
     assert_search("[Aa]p+le", 1)
+  end
+
+  # [issue #38] #1
+  def test_it_can_search_word_in_specified_entries
+    repo = Textrepo::FileSystemRepository.new(@config_ro)
+    result = repo.search("ruby", "202001")
+    assert_operator 2, :<=, result.size
+
+    result = repo.search("swift", "20200101010003")
+    assert_operator 1, :<=, result.size
+
+    result = repo.search("^\\-\\ i", "20200101010000")
+    assert_operator 7, :<=, result.size
   end
 
   def test_it_fails_with_inappropriate_options
